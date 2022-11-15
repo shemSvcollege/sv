ef23fb084fd3824372edc171fe046bb935ca0efc
shem bar <shem@svcollege.co.il>
2022-11-15 15:39:29 +0200

    first

---
+++ src/TestDescription/testInfo.js
@@ -0,0 +1,22 @@
+
+
+export const tests = [{
+    title:'qa',
+    info:'מבחן התאמה לבודקי תוכנה'
+},
+{
+    title:'fullstack',
+    info:'מבחן התאמה למפתחים'
+},
+{
+    title:'cyber',
+    info:'מבחן התאמה לסייבר'
+},
+{
+    title:'gaming',
+    info:'מבחן התאמה לפיתוח משחקים'
+},
+{
+    title:'general',
+    info:'מבחם התאמה כללי'
+},]
